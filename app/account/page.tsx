f365b96e885e961dd3189140113b068a38120a69
Bhotakshe <Bhotakshe@gmail.com>
2025-04-23 18:23:38 +0700

    fix: update profile API and fix authOptions import

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -80,6 +80,7 @@ export default function AccountPage() {
       [name]: value
     }));
   };
+  
 
   const handlePhotoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
